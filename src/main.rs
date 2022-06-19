d533a6bef7437c8c8971e997d25056f2312e4357
DeltaSonic62 <mali.kermali@outlook.com>
2022-06-19 14:31:41 -0400

    Minor design tweaks

--- src/main.rs
+++ src/main.rs
@@ -2,83 +2,76 @@ mod pycsort;
 
 use eframe::{
     egui::{CentralPanel, ProgressBar, Vec2},
-    epi::App,
-    run_native, NativeOptions,
+    run_native, App, NativeOptions,
 };
 
-use pycsort::{PycSort, SPACING};
+use pycsort::PycSort;
 
 impl App for PycSort {
-    fn setup(
-        &mut self,
-        ctx: &eframe::egui::CtxRef,
-        _frame: &eframe::epi::Frame,
-        _storage: Option<&dyn eframe::epi::Storage>,
-    ) {
-        self.font_config(ctx);
-    }
-
-    fn update(&mut self, ctx: &eframe::egui::CtxRef, _frame: &eframe::epi::Frame) {
+    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
         CentralPanel::default().show(ctx, |ui| {
+            ui.add_space(16.);
+
+            // Header
+            ui.heading("PycSort RS");
+
+            ui.add_space(16.);
+            ui.separator();
+            ui.add_space(16.);
+
             ui.vertical_centered(|ui| {
-                // Header
-                ui.heading("PycSort RS");
-                ui.add_space(SPACING);
-
-                ui.separator();
-                ui.add_space(SPACING);
-
                 // Current Folder
                 ui.label(
                     "Found ".to_owned()
                         + &self.num_files.to_string()
                         + " pictures/videos in "
-                        + &self.folder,
+                        + &self.folder.split('/').last().unwrap(),
                 );
-                ui.add_space(SPACING);
+
+                ui.add_space(16.);
 
                 // File Select Button
                 if ui.button("Choose Folder").clicked() {
                     self.change_folder();
                 }
-                ui.add_space(SPACING);
+                ui.add_space(8.);
 
                 // Rescan
                 if ui.button("Rescan Folder").clicked() {
                     self.rescan_folder();
                 }
-                ui.add_space(SPACING);
+            });
 
-                ui.separator();
-                ui.add_space(SPACING);
+            ui.add_space(16.);
+            ui.separator();
+            ui.add_space(16.);
 
+            ui.vertical_centered(|ui| {
                 // Status Label
                 ui.label(&self.status);
-                ui.add_space(SPACING);
+
+                ui.add_space(16.);
 
                 // Sort Button
                 if ui.button("Sort").clicked() {
                     self.sort_files();
                 }
-                ui.add_space(SPACING);
             });
+            ui.add_space(32.);
 
             // Progress Bar
             let progress_bar = ProgressBar::new(self.progress).show_percentage();
             ui.add(progress_bar);
-            ui.add_space(SPACING);
-            ui.separator();
         });
-    }
-
-    fn name(&self) -> &str {
-        "PycSort RS"
     }
 }
 
 fn main() {
-    let app = PycSort::new();
     let mut win_option = NativeOptions::default();
     win_option.initial_window_size = Some(Vec2::new(620.0, 350.0));
-    run_native(Box::new(app), win_option)
+    run_native(
+        "PycSort RS",
+        win_option,
+        Box::new(|cc| Box::new(PycSort::new(cc))),
+    )
 }
